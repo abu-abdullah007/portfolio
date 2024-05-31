722a87ce456c8bb8b83cae0fba96a26c6db28b80
abu abdullah <zabernakib12580@gmail.com>
2024-06-01 00:10:59 +0600

    i have created about page,

--- js/home_page.js
+++ js/home_page.js
@@ -3,10 +3,16 @@ let myName = document.querySelector('#myName');
 
 let paraGraph = document.querySelector('.extra-show');
 
+let seeMore = document.querySelector('#seeMore');
+
 window.addEventListener("load",()=>{
     headingPara.classList.remove('back');
     setTimeout(()=>{
         myName.classList.remove('sideGo');
         paraGraph.style.opacity = "1";
     },300)
+})
+
+seeMore.addEventListener("click",()=>{
+    window.location.assign("../update_Portfolio/about.html");
 })
